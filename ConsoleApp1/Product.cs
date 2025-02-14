008f2fdc8fbc8e8ce870ce26e1f9d318c6368d1a
Koltai Marci <koltai.marton.balazs@students.jedlik.eu>
2025-02-14 08:11:12 +0100

    Main classes added, demand logic started Initial commit

---
+++ ConsoleApp1/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Product
+    {
+        public string name { get; private set; }
+        public float price { get; set; }
+
+        public int supply { get; set; }
+        public int demand { get; set; }
+
+
+        public void determineDemand()
+        {
+
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/Seller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Seller
+    {
+        public string name { get; set; }
+        public List<Product> products { get; set; }
+
+
+    }
+}
